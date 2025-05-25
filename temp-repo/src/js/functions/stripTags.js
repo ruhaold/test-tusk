367c35c9903d320e8a5d231b210d69768948797f
Rinat <155980443+ruhaold@users.noreply.github.com>
2025-05-26 01:19:08 +0500

    s

--- temp-repo/src/js/functions/stripTags.js
+++
@@ -1,9 +0,0 @@
-/**
- * Вырезает html теги из строки
- *
- * @param {string} str целевая строка
- * @return {string} возвращает строку без html тегов
- * */
-export const stripTags = (str = '') => {
-  return str?.replace(/<\/?[^>]+>/gi, '')
-}
--- temp-repo/src/ui/listing/Listing.class.js
+++
@@ -1,5 +0,0 @@
-export class ListingClass {
-  constructor(init = {}) {
-    this.current_page = init?.current_page || 1
-  }
-}
