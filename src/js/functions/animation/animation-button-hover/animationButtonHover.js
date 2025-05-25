e31b8bae332a4ad61a26fe94e9db00d2b93081fd
Ринат Ярмухаметов <rinat.yarmukhametov.work@gmail.com>
2025-05-26 01:08:47 +0500

    Первоначалеуtest

---
+++ src/js/functions/animation/animation-button-hover/animationButtonHover.js
@@ -0,0 +1,14 @@
+import Cursor from './cursor';
+import ButtonCtrl from './buttonCtrl.js';
+
+export const animationButtonHover = () => {
+  // Анимация кнопок отключена
+  /*
+  // initialize custom cursor
+  const cursor = new Cursor(document.querySelector('.cursor'));
+  const button = new ButtonCtrl(document.querySelector('.btn-common'));
+
+  button.on('enter', () => cursor.enter());
+  button.on('leave', () => cursor.leave());
+  */
+}
